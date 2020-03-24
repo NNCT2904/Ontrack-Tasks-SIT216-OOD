f8496c85f5dee093e5afea1fd1fe49d0622373ba
Thanh Nguyen <44821915+NNCT2904@users.noreply.github.com>
2020-03-24 11:03:25 +1100

    Inheriting codes from tsk2.2

---
+++ tsk3.2/tsk3.2/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Account
+{
+    public class Account
+    {
+        private decimal _balance;
+        private String _name;
+        public Account(String name, decimal balance)
+        {
+            this._balance = balance;
+            this._name = name;
+        }
+
+
+        //Mutator methods
+        public void Deposit(decimal Ammount)
+        {
+            this._balance += Ammount;
+        }
+
+        public void Withdraw(decimal Ammount)
+        {
+            this._balance -= Ammount;
+        }
+
+        //Accessor methods
+        public void Print()
+        {
+            Console.WriteLine("Account name: " + this._name);
+            Console.WriteLine("Available balance: " + this._balance);
+        }
+        public String Name()
+        {
+            return this._name;
+        }
+    }
+}
