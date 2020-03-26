895213f768f6bbe3f04f78875f395d97a883b209
Thanh Nguyen <nnct2000@gmail.com>
2020-03-26 14:31:57 +1100

    Inheriting from task 3.2, I fordot to do the 3.3

---
+++ tsk3.4/tsk3.4/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Account
+{
+    public class Account
+    {
+        private decimal _balance;
+        private String _name;
+        public Account(String name, decimal balance)
+        {
+            this._balance = balance;
+            this._name = name;
+        }
+
+        //Accessor methods
+        public void Print()
+        {
+            Console.WriteLine("Account name: " + this._name);
+            Console.WriteLine("Available balance: " + this._balance);
+        }
+        public String Name() => this._name;
+        public Decimal Balance() => this._balance;
+
+        //Mutator methods
+        public bool Deposit(decimal ammount)
+        {
+            bool deposited = false;
+            if (ammount < 0)
+            {
+                Console.WriteLine("The deposit ammount must not be negative");
+                deposited = false;
+            }
+            else if (ammount >= 0)
+            {
+                this._balance += ammount;
+                deposited = true;
+            }
+            return deposited;
+        }
+
+        public bool Withdraw(decimal ammount)
+        {
+            bool withdraw = false;
+            if (ammount < 0)
+            {
+                Console.WriteLine("The deposit ammount must not be negative");
+                withdraw = false;
+            }
+            else if (ammount >= 0)
+            {
+                this._balance -= ammount;
+                withdraw = true;
+            }
+            return withdraw;
+        }
+    }
+}
---
+++ tsk3.4/tsk3.4/BankSystem.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+public enum MenuOptions
+{
+    Deposit = 1,
+    Withdraw = 2,
+    Print = 3,
+    Quit = 4
+
+}
+
+
+namespace Account
+{
+    class BankSystem
+    {
+        static void Main(string[] args)
+        {
+            Account ncng = new Account("Thanh Nguyen", 99235);
+
+            //print out the UI
+            MenuOptions option = ReadUserInput();
+
+
+            switch (option)
+            {
+                case MenuOptions.Deposit:
+                    {
+                        Console.WriteLine(Convert.ToString(option));
+                        DoDeposit(ncng);
+                        break;
+                    }
+                case MenuOptions.Withdraw:
+                    {
+                        Console.WriteLine(Convert.ToString(option));
+                        DoWithdraw(ncng);
+                        break;
+                    }
+                case MenuOptions.Print:
+                    {
+                        Console.WriteLine(Convert.ToString(option));
+                        DoPrint(ncng);
+                        break;
+                    }
+                case MenuOptions.Quit:
+                    {
+                        Console.WriteLine(Convert.ToString(option));
+                        break;
+                    }
+            }
+        }
+        static MenuOptions ReadUserInput()
+        {
+            int userInput;
+
+            //Print out the menu options, add more options "MenuOptions" enum above will print more options
+            Console.WriteLine("Bank System Options");
+            foreach (MenuOptions choices in Enum.GetValues(typeof(MenuOptions)))
+            {
+                Console.WriteLine("-{0}. {1}", (int)choices, choices);
+            }
+
+            //User input validation, stricted to range (1-4)
+            do
+            {
+                userInput = InputToInt(Console.ReadLine());
+                if (userInput <= 0 || userInput >= 4)
+                {
+                    Console.WriteLine("Unknown option, try again!");
+                }
+            } while (userInput <= 0 || userInput >= 4);
+
+            //Return the option based on the user input, expected 1 to 4
+            return (MenuOptions)userInput;
+        }
+
+        // These function check the user input
+        static int InputToInt(string inputNumberAsString)
+        {
+            int inputNumber;
+            while (!int.TryParse(inputNumberAsString, out inputNumber))
+            {
+                Console.WriteLine("This is not quite a number");
+                inputNumberAsString = Console.ReadLine();
+            }
+            return inputNumber;
+
+        }
+        static decimal InputToDec(string inputNumberAsString)
+        {
+            decimal inputNumber;
+            while (!decimal.TryParse(inputNumberAsString, out inputNumber))
+            {
+                Console.WriteLine("This is not quite a number");
+                inputNumberAsString = Console.ReadLine();
+            }
+            return inputNumber;
+
+        }
+
+        // Methodd
+        static void DoDeposit(Account account)
+        {
+            Console.WriteLine("Input ammount");
+            if (account.Deposit(InputToDec(Console.ReadLine())))
+            {
+                Console.WriteLine("Deposit sucessful, your new balance is " + account.Balance());
+            }
+        }
+        static void DoWithdraw(Account account)
+        {
+            Console.WriteLine("Input ammount");
+            if (account.Withdraw(InputToDec(Console.ReadLine())))
+            {
+                Console.WriteLine("Widthdraw sucessful, your new balance is " + account.Balance());
+            }
+        }
+        static void DoPrint(Account account)
+        {
+            account.Print();
+        }
+    }
+}
