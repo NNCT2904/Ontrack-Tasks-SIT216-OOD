fc723522b48bd59fb30480e129700faa1da7d05e
Thanh Nguyen <nnct2000@gmail.com>
2020-04-04 16:52:15 +1100

    Getting ready for 5.1

---
+++ tsk5.1/ZooPark/Animals.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ZooPark
+{
+    public class Animals
+    {
+        private string name, diet, location, colour;
+        private double weight;
+        private int age;
+
+        public Animals(string name, string diet, string location, double weight, int age, string colour)
+        {
+            this.name =     name;
+            this.diet =     diet;
+            this.location = location;
+            this.weight =   weight;
+            this.age =      age;
+            this.colour = colour;
+        }
+
+        public void Eat()
+        {
+            //Code for eat
+        }
+
+        public void Sleep()
+        {
+            //Code for sleep
+        }
+
+        public void MakeNoise()
+        {
+            //Code for making noise
+        }
+    }
+}
