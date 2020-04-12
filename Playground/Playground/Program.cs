7a6dfee56c704237cb2cefafdcfc4c304c4432bb
Thanh Nguyen <nnct2000@gmail.com>
2020-04-12 20:47:32 +1000

    Playground for testing codes

---
+++ Playground/Playground/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static bool check(decimal value)
+        {
+            bool check;
+            if (value > 0)
+            {
+                check = true;
+            }
+            else
+            {
+                check = false;
+            }
+
+            return check;
+        }
+    }
+}
