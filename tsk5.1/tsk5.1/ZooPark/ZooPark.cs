e24bd16f1e35789066a7611431cac4a5a1df18c2
ncng <Nnct2000@gmail.com>
2020-04-21 15:22:03 +1000

    Cleanup a bit

---
+++ tsk5.1/tsk5.1/ZooPark/ZooPark.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ZooPark
+{
+    class ZooPark
+    {
+        static void Main(string[] args)
+        {
+            Animals williamWolf = new Animals("William the Wolf", "Meat", "Dog Village", 50.6, 9, "Grey");
+            Animals tonyTiger = new Animals("Tony the Tiger", "Meat", "Cat Land", 110, 6, "Orange and White");
+            Animals edgarEagle = new Animals("Edgar the Eagle", "Fish", "Bird Mania", 20, 15, "Black");
+
+        }
+    }
+}
