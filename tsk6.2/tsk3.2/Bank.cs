a081a0c0f846ca0f6c7360dbcfadc184f6f86e79
Thanh Nguyen <nnct2000@gmail.com>
2020-04-26 21:17:28 +1000

    Finished 6.2

---
+++ tsk6.2/tsk3.2/Bank.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Account
+{
+    public class Bank
+    {
+        private List<Account> _accounts = new List<Account>();
+        public Bank()
+        {
+        }
+
+        public void AddAccount(Account account)
+        {
+            this._accounts.Add(account);
+        }
+
+        public Account Search(string name)
+        {
+            foreach (var account in this._accounts)
+            {
+                if (account.Name == name)
+                {
+                    return account;
+                }
+            }
+            //Return null if can not find any account
+            return null;
+        }
+
+        public void ExecuteTransaction(WithdrawTransaction transaction)
+        {
+            transaction.Execute();
+        }
+
+        public void ExecuteTransaction(DepositTransaction transaction)
+        {
+            transaction.Execute();
+        }
+
+        public void ExecuteTransaction(TransferTransaction transaction)
+        {
+            transaction.Execute();
+        }
+    }
+}
