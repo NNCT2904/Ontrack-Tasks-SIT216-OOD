fe1023c5c475006d37c486fab9993a6c513edd61
ncng <Nnct2000@gmail.com>
2020-05-08 11:18:05 +1000

    Initiate task 7.1

---
+++ tsk7.1/AbstractedTransaction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractedTransaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
